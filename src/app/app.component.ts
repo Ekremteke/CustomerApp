ae12f70f766033cab7db17788404c2c9147f9eeb
Ekrem <ekremteke.uk@gmail.com>
2024-02-13 14:28:27 +0000

    custemer file was created

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Customer } from './app.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.view.html',
+
+})
+export class AppComponent {
+  title = 'Week 7 Introduction';
+  CustomerModel: Customer = new Customer(); 
+  CustomerModels :Array<Customer> = new Array<Customer>();
+  Add(){
+    this.CustomerModels.push(this.CustomerModel);
+    this.CustomerModel = new Customer();// clear UI
+  }
+}
